ac5f3d2c608fb2fa1797626a5152e7b97684696a
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-04-19 10:29:16 -0700

    comment

--- 2_cannotAccess.js
+++ 2_cannotAccess.js
@@ -6,3 +6,6 @@ console.log( firstName );
 console.log( lastName );
 
 let lastName = "Miller";
+
+// lastName cannot be accessed because it has not been defined at the time that the console tries to log it  
+// move the line up to line 4 in order to declare it be able to console log it. 
