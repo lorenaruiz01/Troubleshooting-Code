10a30911c9b69bdb97e677a67d237928774d69a3
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-04-19 10:54:46 -0700

    fizzbuzz code

--- 7_fiizzBuzzChallenge.js
+++ 7_fiizzBuzzChallenge.js
@@ -1,2 +1,14 @@
 // Logical error "Order of operators"
 // Fizz buzz challenge
+
+function fizzbuzz (limit){
+    for (let i = 1; i <= limit; i ++ ){
+        if (i % 3 ===0 && i % 5 === 0 ){
+            console.log("FizzBuzz")
+        } else if (i % 3 === 0){
+            console.log("Fizz");
+        } else if (i % 5 == 0 ){
+        console.log("buzz");
+        }
+    }
+}
