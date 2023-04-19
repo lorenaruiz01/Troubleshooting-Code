31dc35d7cae388741ec42cacd9b255cc2c81cbcf
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-04-19 10:41:29 -0700

    add languages and proficiency

--- 4_propertyOfUndefined.js
+++ 4_propertyOfUndefined.js
@@ -7,3 +7,14 @@ let student = {
 
 console.log( student.fullName );
 console.log( student.languages.proficient );
+
+// .fullName is not defined in variable
+//  Cannot read properties of undefined (reading 'proficient')
+// cannot access a property of a value that is not defined or unknown
+
+let student = {
+    firstName : "Alex",
+    lastName : "Miller",
+    languages: {
+        proficiency: "master";
+};
